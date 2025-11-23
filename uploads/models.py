5cfa3a490415c30060210773e6e76297aced4fe8
ShinyBread <shinybreadjp@gmail.com>
2025-11-22 21:20:14 -0300

    changesxd

--- uploads/models.py
+++ uploads/models.py
@@ -2,16 +2,13 @@ from django.db import models
 from django.utils.crypto import get_random_string
 
 class Image(models.Model):
-    # El código de la URL (ej: 'x9s')
+   
     short_code = models.CharField(max_length=10, unique=True, blank=True, db_index=True)
-    
-    # La imagen (Django la enviará a Cloudinary automáticamente)
     image = models.ImageField(upload_to='shinies/')
-    
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        # Si no tiene código, generamos uno de 5 caracteres
+        
         if not self.short_code:
             self.short_code = get_random_string(5)
         super().save(*args, **kwargs)
